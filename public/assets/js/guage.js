b21c5c864faf171fa5a041e4148d161f2c71c48e
topdev60 <topdev60@gmail.com>
2022-08-11 04:55:19 +0200

    guage complete

--- public/assets/js/guage.js
+++ public/assets/js/guage.js
@@ -29,8 +29,8 @@ var guage = function (value, title, position) {
         width: 300,
         height: 250,
         margin: { t: 25, r: 25, l: 25, b: 25 },
-        paper_bgcolor: "lavender",
-        font: { color: "darkblue", family: "Arial" }
+        paper_bgcolor: "2b2e43",
+        font: { color: "white", family: "Arial" }
       };
 
     position = position == undefined ? '' : position;
